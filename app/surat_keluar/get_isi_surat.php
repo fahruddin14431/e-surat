ecdad98945808bd7d96c220c3efcdf90501f2c2e
fahruddin <yusufudin14431@gmail.com>
2018-04-24 22:22:43 +0700

    rev login tittle and fix bug ajax

--- app/surat_keluar/get_isi_surat.php
+++ app/surat_keluar/get_isi_surat.php
@@ -2,11 +2,14 @@
 
 include "../helper/crud.php";
 
-$id_jenis_surat = $_POST['id_jenis_surat'];
+$id_jenis_surat = (!empty($_POST['id_jenis_surat'])?$_POST['id_jenis_surat']:"");
 
-$crud   = new Crud();
-$result = $crud->view("SELECT isi_surat FROM tb_jenis_surat WHERE id_jenis_surat='$id_jenis_surat'")[0];
-$data   = $result['isi_surat'];
+$data   = "";
+if($id_jenis_surat){
+    $crud   = new Crud();
+    $result = $crud->view("SELECT isi_surat FROM tb_jenis_surat WHERE id_jenis_surat='$id_jenis_surat'")[0];
+    $data   = $result['isi_surat'];
+}
 
 echo json_encode(array("data"=>$data));
 
